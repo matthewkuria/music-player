3139f98fb2b17fe2f2d2ef6ce443f2b5a62ded31
matthewkuria <mathewkwachira@gmail.com>
2024-08-10 20:40:54 +0300

    Rename File in the components

---
+++ src/components/ForgotPassword/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC49qdbhsXm5m6Y8whiS5MFyuyUJfuSX7w",
+  authDomain: "music-player-25bdb.firebaseapp.com",
+  projectId: "music-player-25bdb",
+  storageBucket: "music-player-25bdb.appspot.com",
+  messagingSenderId: "160200136042",
+  appId: "1:160200136042:web:4b7dab2cc311bbf359c113"
+};
+  
+  export default firebaseConfig;
