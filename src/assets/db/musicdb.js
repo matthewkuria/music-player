b48b25af5bdaa89a75fde6058aa3bf7d84d9f927
matthewkuria <mathewkwachira@gmail.com>
2024-08-14 20:08:06 +0300

    Rebuild the card component again

---
+++ src/assets/db/musicdb.js
@@ -0,0 +1,21 @@
+const playlist = [
+  {
+    title: "Song 1",
+    artist: "Artist 1",
+    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+    cover: "https://via.placeholder.com/345x140.png?text=Album+Cover+1"
+  },
+  {
+    title: "Song 2",
+    artist: "Artist 2",
+    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
+    cover: "https://via.placeholder.com/345x140.png?text=Album+Cover+2"
+  },
+  {
+    title: "Song 3",
+    artist: "Artist 3",
+    src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3",
+    cover: "https://via.placeholder.com/345x140.png?text=Album+Cover+3"
+  }
+];
+export default playlist;
